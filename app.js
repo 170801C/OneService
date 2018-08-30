67f7d2cf856b92224d39b0053d9bb558f7e31980
Joanne <170801C@mymail.nyp.edu.sg>
2018-08-30 18:52:44 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,83 @@
+// Import modules
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Import multer
+var multer = require('multer');
+var upload = multer({ dest:'./public/uploads/', limits: {fileSize: 150000000000000, files:1} });
+
+// Modules to store session
+var myDatabase = require('./server/controllers/database');
+var expressSession = require('express-session');
+var SessionStore = require('express-session-sequelize')(expressSession.Store);
+var sequelizeSessionStore = new SessionStore({
+    db: myDatabase.sequelize,
+});
+
+// Import controllers
+
+
+
+// Create server
+var app = express();
+var serverPort = 3000;
+var httpServer = require('http').Server(app);
+
+
+// Use middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true,
+    sourceMap: true
+}));
+
+
+// Setup public directory (assets)
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public',express.static('public'));
+
+
+// Setup View engine (EJS)
+app.set('views', path.join(__dirname, 'server/views/pages'));
+app.set('view engine', 'ejs');
+
+
+// Routes
+
+
+
+// Errors catch and error handlers
+// Catch 404 error and forward it to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Catch production errors and forward it to error handler. No stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+// Setup Server
+app.set('port', serverPort);
+var server = httpServer.listen(app.get('port'), function () {
+    console.log('HTTP Server listening on port' + server.address().port);
+});
+
+
+// Exports: app
+module.exports = app;
