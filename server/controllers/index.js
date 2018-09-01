d8c136713b291a987d8e14143ba8ac22933023cb
HanJiyao <hanjiyao0@gmail.com>
2018-09-02 03:28:55 +0800

    fix index

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,16 +1,16 @@
-var Facilities = require('../models/Facilities')
-
+var Models = require('../models')
+var Facilities = Models.Facilities
 exports.show = function (req, res) {
-    sequelize.query(`select * from Facilities`, { model: Facilities})
-        .then((facility) => {
-            res.render('index', {           // Render index.ejs template 
-                title: 'One Booking',        // Pass data to title field in template
-                facilities: facility
-            })
-        }) 
-        .catch((err) => {
-            return res.status(400).send({
-                message: err
-            });
+    Models.sequelize.query(`select * from Facilities`, { model: Facilities})
+    .then((facility) => {
+        res.render('index', {           // Render index.ejs template 
+            title: 'One Booking',        // Pass data to title field in template
+            facilities: facility
+        })
+    }) 
+    .catch((err) => {
+        return res.status(400).send({
+            message: err
         });
+    });
 };
