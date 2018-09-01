a8c6b8834034e2d0b4115bcdcafd544eb682fb35
Joanne <170801C@mymail.nyp.edu.sg>
2018-09-01 18:56:09 +0800

    Created Bookings model and updated Facilities model

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,16 @@
+var Facilities = require('../models/Facilities')
+
 exports.show = function (req, res) {
-    res.render('index', {           // Render index.ejs template 
-        title: 'OneService',        // Pass data to title field in template
-    });
+    sequelize.query(`select * from Facilities`, { model: Facilities})
+        .then((facility) => {
+            res.render('index', {           // Render index.ejs template 
+                title: 'OneService',        // Pass data to title field in template
+                facilities: facility
+            })
+        }) 
+        .catch((err) => {
+            return res.status(400).send({
+                message: err
+            });
+        });
 };
