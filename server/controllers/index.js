3f95ff877f9b684d7bf2858604af2fd7a6973048
HanJiyao <hanjiyao0@gmail.com>
2018-09-02 03:13:23 +0800

    title change

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,10 @@
 var Facilities = require('../models/Facilities')
 
 exports.show = function (req, res) {
-<<<<<<< HEAD
-    res.render('index', {           // Render index.ejs template 
-        title: 'One Booking',        // Pass data to title field in template
-    });
-=======
     sequelize.query(`select * from Facilities`, { model: Facilities})
         .then((facility) => {
             res.render('index', {           // Render index.ejs template 
-                title: 'OneService',        // Pass data to title field in template
+                title: 'One Booking',        // Pass data to title field in template
                 facilities: facility
             })
         }) 
@@ -18,5 +13,4 @@ exports.show = function (req, res) {
                 message: err
             });
         });
->>>>>>> master
 };
